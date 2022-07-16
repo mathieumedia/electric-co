84c06ddff4fb3d796dfa87fcb874cd5bbed913df
Guy Mathieu <57466994+GuyEMathieu@users.noreply.github.com>
2022-07-16 08:03:43 -0400

    Created Initial navigation tree in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import SimpleBar from 'simplebar-react';
+import {BrowserRouter as Router, Routes, Route, BrowserRouter} from 'react-router-dom'
 
+
+
+// #region --------- COMPONENTS --------------
+import LoginPage from './pages/authPages/LoginPage';
+import NotFound from './pages/authPages/NotFound';
+import PrivateRoute from './pages/authPages/PrivateRoute'
+
+import Admin from './pages/adminPages/Admin'
+// #endregion
 function App() {
     return (
         <div className="App">
-            Electric Co
+            <SimpleBar style={{maxHeight:'100vh', height: '100vh'}}>
+                <Router>
+                    <Routes>
+                        <Route path='/' element={<LoginPage  />} />
+                        <Route path='*' element={<NotFound  />} />
+
+                        <Route path='/admin' element={<PrivateRoute  />}>
+                            <Route path='/admin' element={<Admin  />} />
+                        </Route>
+                    </Routes>
+                </Router>
+
+            </SimpleBar>
         </div>
     );
 }
