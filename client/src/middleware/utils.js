01c6e59e7a0d12095699d1a57d0cc599b7f772b6
Guy Mathieu <57466994+GuyEMathieu@users.noreply.github.com>
2022-07-26 20:55:02 -0400

    Created New Bill UI in Billing Dash

--- client/src/middleware/utils.js
+++ client/src/middleware/utils.js
@@ -54,3 +54,20 @@ export function getName(arr, id){
     return ''
 }
 
+export function billingMonths () {
+    return [
+        {value: '01', label: 'January'},
+        {value: '02', label: 'February'},
+        {value: '03', label: 'March'},
+        {value: '04', label: 'April'},
+        {value: '05', label: 'May'},
+        {value: '06', label: 'June'},
+        {value: '07', label: 'July'},
+        {value: '08', label: 'August'},
+        {value: '09', label: 'September'},
+        {value: '10', label: 'October'},
+        {value: '11', label: 'November'},
+        {value: '12', label: 'December'}
+    ]
+}
+
--- client/src/pages/adminPages/AdminCustomerBilling.jsx
+++ client/src/pages/adminPages/AdminCustomerBilling.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import {useState} from 'react'
+import {
+    Grid, TextField, MenuItem, Button, Paper
+} from '@mui/material'
+import * as utils from '../../middleware/utils'
 
 export default function AdminCustomerBilling() {
-  return (
-    <div>AdminCustomerBilling</div>
-  )
+    const [newBill, setNewBill] = useState({year: new Date().getFullYear(), month: ''})
+
+    const handleCreateNewBill = () => {
+        utils.BeautifyAlert(newBill)
+    }
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={12} md={8}>
+                <Paper sx={{p: 1}}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={4}>
+                            <TextField
+                                label="Year" value={newBill?.year}
+                                select onChange={e => setNewBill({...newBill, year: e.target.value})}
+                            >
+                                <MenuItem disabled>Select Year</MenuItem>
+                                {[2022, 2021, 2020].map(_year => (
+                                    <MenuItem key={_year} value={_year}>{_year}</MenuItem>
+                                ))}
+                            </TextField>
+                        </Grid>
+
+                        <Grid item xs={12} md={4}>
+                            <TextField
+                                label="Month" value={newBill?.month}
+                                select onChange={e => setNewBill({...newBill, month: e.target.value})}
+                            >
+                                <MenuItem disabled>Select Month</MenuItem>
+                                {utils?.billingMonths().map(_month => (
+                                    <MenuItem key={_month.value} value={_month.value}>{_month.label}</MenuItem>
+                                ))}
+                            </TextField>
+                        </Grid>
+                        <Grid item xs={12} md={4} sx={{alignItems: 'center', display: 'flex'}}>
+                            <Button fullWidth onClick={handleCreateNewBill}>Create Bill</Button>
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Grid>
+        </Grid>
+    )
 }
